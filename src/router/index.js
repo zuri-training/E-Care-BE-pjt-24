838e656665759d76e8dadf16234f206f0f7cda22
AKPAKPAN ANIEKAN-ABASI <akpakpan2535@bazeuniversity.edu.ng>
2021-06-16 10:47:57 +0100

    fresh start

--- src/router/index.js
+++
@@ -1,5 +0,0 @@
-const baseRoute = require('../core/routerConfig');
-
-baseRoute.get('/', (req, res) => res.status(200).send('<code>simple blog backend api running...<a target="_blank" href="https://documenter.getpostman.com/view/12307581/TzeTHUgF" style="text-decoration: none; cursor: pointer; color: black; font-weight: bold">&lt;View Documentation/&gt;</a></code>'));
-
-module.exports = baseRoute;
--- utils/mailgun.js
+++
@@ -1,6 +0,0 @@
-const mailgun = require('mailgun-js');
-const { MAILGUN_APIKEY, MAILGUN_DOMAIN } = require('../src/core/config');
-
-const msg = mailgun({ apiKey: MAILGUN_APIKEY, domain: MAILGUN_DOMAIN });
-
-module.exports = msg;
